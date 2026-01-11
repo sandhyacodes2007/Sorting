7bce5bfcc0f31d6356d216b7631faba21981c1bb
Sandhya Verma <sandhyapatel0134@gmail.com>
2026-01-11 22:22:32 +0530

    Add files via upload

---
+++ Bubble_Sort_in_Sorting.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Bubble_Sort(int arr[],int n){
+    //for outer loop.
+    for(int i=n-1; i>=0; i--){
+        
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+
+            }
+        }
+    }
+}
